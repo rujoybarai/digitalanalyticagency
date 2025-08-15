ae98ff6fb5634dae8505d470abbfde16148a6533
rujoybarai <rujoybarai13@gmail.com>
2025-08-15 12:22:59 +0600

    Rujoy Barai

--- sign/sign.js
+++ sign/sign.js
@@ -9,7 +9,7 @@ loginid.addEventListener("click",(e)=>{
     e.preventDefault();
     
    Box1.classList.add("hide");
-   Box2.classList.remove("hide")
+   Box2.classList.remove("hide");
 
 console.log(loginid);
 
@@ -20,7 +20,7 @@ regid.addEventListener("click",(e)=>{
 
     e.preventDefault();
    Box2.classList.add("hide");
-   Box1.classList.remove("hide")
+   Box1.classList.remove("hide");
 
 
 
